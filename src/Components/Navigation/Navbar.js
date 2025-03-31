98e81530d106e7e7dbf2f2d7554b14159137b39a
unknown <sachin@0307>
2025-03-31 20:49:07 +0530

    Resolved: totalCartSize error

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -178,7 +178,7 @@ export default function Navbar({ isLoggedIn, location, setLocationModal,isAdmin 
                   className="relative p-2 rounded-full transition"
                 >
                   <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-semibold w-5 h-5 flex items-center justify-center rounded-full">
-                    {cartSum.data.totalCartSize}
+                    {cartSum?.data?.totalCartSize||0}
                   </span>
                   <i className="fa-solid fa-cart-shopping"></i>
                 </Link>
